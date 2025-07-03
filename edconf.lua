999b16a15b459e448a088ef5666d24a536f295e8
Matěj Cepl <mcepl@cepl.eu>
2025-07-03 22:28:03 +0200

    feat: add Apache 2.0 license

--- edconf.lua
+++ edconf.lua
@@ -1,3 +1,20 @@
+-- Copyright 2019-2023 Frank Seifferth
+-- Copyright 2021-2023 Florian Fischer
+-- Copyright 2025 Matěj Cepl
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+
 require "vis"
 local ec = require "editorconfig"
 local M = {}
